04ede0e52d39bb07e910d46347d7dce06ed3735e
Eser Karaceper <52802861+esero88@users.noreply.github.com>
2021-09-08 10:45:29 +0300

    Update Student Advisor.sql
    One small note has been added for clarity of the query.

--- Student Advisor.sql
+++ Student Advisor.sql
@@ -1,3 +1,3 @@
 select std.roll_number, std.name 
 from student_information std, faculty_information fi 
-where std.advisor = fi.employee_id and (fi.gender = 'M' and fi.salary > 15000 or fi.gender = 'F' and fi.salary > 20000) 
+where std.advisor = fi.employee_id and (fi.gender = 'M' and fi.salary > 15000 or fi.gender = 'F' and fi.salary > 20000) -- In SQL server AND operator always takes precedence over to OR operator.
